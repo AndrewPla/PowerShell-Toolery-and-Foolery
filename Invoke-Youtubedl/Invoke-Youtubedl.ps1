90c119b790f3dfe30bcd9cb1f9b8a126c957c645
AndrewPla <APla@igymarinas.com>
2018-06-13 13:27:13 -0400

    Improvements to Invoke-Youtubedl

--- Invoke-Youtubedl/Invoke-Youtubedl.ps1
+++ Invoke-Youtubedl/Invoke-Youtubedl.ps1
@@ -19,25 +19,54 @@ function Invoke-YoutubeDl {
     [CmdletBinding()]
     param
     ( 
+        # Set Path to Download folder. 
+        #TODO add download folder for windows 7 and conditionally choose which one to use    
         [string]
-        $Path = "$ScriptDirectory\downloads",
-		
+        $Path = (Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders').'{7D83EE9B-2244-4E70-B1F5-5393042AF1E4}',
+        
+        
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [ValidatePattern('http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?')]
         [string]
         $url,
 
+        [Parameter(ParameterSetName = 'AudioOnly')]
         [switch]
-        $AudioOnly
+        $AudioOnly,
+
+        [Parameter(ParameterSetName = 'AudioOnly')]
+        [ValidateSet("best", "aac", "flac", "mp3", "m4a", "opus", "vorbis", "wav")]
+        $AudioFormat = "mp3"
     )
 
     begin {
-		
+        $ErrorActionPreference = 'stop'
+        Write-Verbose "Bound Parameters: $PSBoundParameters"
+        try { Test-Path $env:ChocolateyInstall }
+        catch { Write-Error "Chocolatey install not found. Visit https://chocolatey.org/docs/installation for more information" }
+        
     }
     process {
-        youtube-dl.exe -o "$path/%(title)s.%(ext)s" $url
+        if ($PsCmdlet.ParameterSetName -eq 'AudioOnly') {
+            Write-Verbose 'Detected AudioOnly ParameterSetName'
+            try {
+                if (!(Test-Path "$env:chocolateyinstall\lib\ffmpeg")) {
+                    throw 'Audio extraction requires ffmpegs. To install, open PS as admin and run: Choco install ffmpegs'
+                }
+            }
+            catch {
+                $_
+            }
+         youtube-dl.exe -o "$path/%(title)s.%(ext)s" $url -x --audio-format $AudioFormat
+
+        }
+        else {
+            youtube-dl.exe -o "$path/%(title)s.%(ext)s" $url
+        }   
     }
+   
+
     end {
 		
     }
