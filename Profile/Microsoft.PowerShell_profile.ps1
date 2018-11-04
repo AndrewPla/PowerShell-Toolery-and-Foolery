53677e275ee5b6bf0f18b76560ef95d31e428bb7
AndrewPla <32366464+AndrewPla@users.noreply.github.com>
2018-11-04 08:39:16 -0500

    updated profile

--- Profile/Microsoft.PowerShell_profile.ps1
+++ Profile/Microsoft.PowerShell_profile.ps1
@@ -1,40 +1,41 @@
 $PSDefaultParameterValues = @{
-
     'Export-Csv:NoTypeInformation' = $true
-    'Install-Module:Scope' = CurrentUser
-
+    'Install-Module:Scope' = 'CurrentUser'
+    'Out-Default:OutVariable' = 'LastOut'
+    'Connect-Meraki:ApiKey' = (Get-PSFConfigValue merakips.apikey)
 }
 
+
+#region modules
 $modulesToImport = @(
-
     'PSUtil'
-
+    'HistoryPx'
+    'PSConsoleTheme'
 )
 
 ForEach ($module in $modulesToImport) {
-
     $isValid = Get-Module $module -ListAvailable
-
     if ($null -eq $isValid) {Write-Warning "$module is not found in Modules path: $($env:PSModulePath)"}
-
+    Import-Module $module
 }
+#endregion modules
 
 
-
+#region functions
 $Functions = Get-ChildItem $PSScriptRoot\Functions\*.ps1 -ErrorAction SilentlyContinue
 
 foreach ($import in $Functions) {
-
     Try {
-
         . $import.fullname
-
     }
-
     Catch {
-
         Write-Error "Failed to import function $($import.fullname): $_"
-
     }
-
 }
+#endregion functions
+
+
+#region one-off settings
+Set-ConsoleTheme Flat
+Set-PSReadlineOption -BellStyle None
+#endregion one-off settings
